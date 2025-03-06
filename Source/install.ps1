fa0c25cecc511f37d3558cc04bea65f2bb354c49
MrMeeb <16963756+MrMeeb@users.noreply.github.com>
2025-03-06 15:35:49 +0000

    Move to install.ps1 since install.cmd was causing major pain

---
+++ Source/install.ps1
@@ -0,0 +1,48 @@
+param(
+    [Parameter(Mandatory)]
+    [string]$Action,
+    [Parameter(Mandatory)]
+    [string]$AppName,
+    [Parameter()]
+    [string]$Params
+ )
+ 
+$LOGROOT="${env:ProgramFiles}\CAW\IntuneLogs\$AppName"
+
+New-Item -ItemType Directory -Path "${env:ProgramFiles}\CAW\IntuneLogs" -Name $AppName -Force
+Start-Transcript -path $LOGROOT\install.ps1.log -append
+
+Write-Host "Action is $Action"
+Write-Host "App is $AppName"
+
+$NewAcl = Get-Acl -Path $LOGROOT
+# Set properties
+$identity = "BUILTIN\Users"
+$fileSystemRights = "FullControl"
+$type = "Allow"
+# Create new rule
+$fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
+$newParams = @{
+  TypeName     = 'System.Security.AccessControl.FileSystemAccessRule'
+    ArgumentList = $fileSystemAccessRuleArgumentList
+}
+$fileSystemAccessRule = New-Object @newParams
+# Apply new rule
+$NewAcl.SetAccessRule($fileSystemAccessRule)
+
+Set-Acl -Path $LOGROOT -AclObject $NewAcl
+
+Invoke-Webrequest -uri https://raw.githubusercontent.com/MrMeeb/Intune-Choco-Universal-App-Installer/refs/heads/develop/Online/install.ps1 -outfile "$env:TMP\install-$AppName.ps1"
+
+if ($Params.Length -gt 0) { 
+
+  Powershell.exe -ExecutionPolicy ByPass -file "$env:TMP\install-$AppName.ps1" -Action install -AppName $AppName -Params "$Params"
+
+} else {
+
+  Powershell.exe -ExecutionPolicy ByPass -file "$env:TMP\install-$AppName.ps1" -Action install -AppName $AppName
+
+}
+
+
+Stop-Transcript
